ad446e70d77e300896efd540335b5b7a4444faeb
Nirvana <kimqqyun@gmail.com>
2022-06-15 00:03:01 +0900

    Unittest setUp

--- functional_test.py
+++ functional_test.py
@@ -1,11 +1,24 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
+import unittest
 
+class NewVisitorTest(unittest.TestCase):
 
-chrome_options = webdriver.ChromeOptions()
-# 설치되어 있는 크롬으로 설정
-browser = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=chrome_options)
-browser.get("http://google.com")
+    def setUp(self):
+        """설치되어 있는 크롬으로 설정"""
+        chrome_options = webdriver.ChromeOptions()
+        self.browser = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=chrome_options)
+        self.browser.implicitly_wait(3)
 
-assert 'Google' in browser.title
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_can_start_a_list_and_retrieve_it_later(self):
+        self.browser.get("http://localhost:8000")
+
+        self.assertIn('To-Do', self.browser.title)
+        self.fail('Finish the test!')
+
+if __name__ == '__main__':
+    unittest.main(warnings='ignore')
