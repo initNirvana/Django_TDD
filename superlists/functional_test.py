d45a37fd58fac8bc1706f912ae6c4c34b04b510c
Nirvana <kimqqyun@gmail.com>
2022-06-26 01:18:24 +0900

    기능 테스트 작업 아이템 확인

--- superlists/functional_test.py
+++ superlists/functional_test.py
@@ -1,11 +1,49 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
+import unittest
+
+class NewVisitorTest(unittest.TestCase):
+
+    def setUp(self):
+        """설치되어 있는 크롬으로 설정"""
+        chrome_options = webdriver.ChromeOptions()
+        self.browser = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=chrome_options)
+        self.browser.implicitly_wait(3)
+
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_can_start_a_list_and_retrieve_it_later(self):
+        self.browser.get("http://localhost:8000")
 
 
-chrome_options = webdriver.ChromeOptions()
-# 설치되어 있는 크롬으로 설정
-browser = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=chrome_options)
-browser.get("http://google.com")
+        # 타이틀 확인
+        self.assertIn('To-Do', self.browser.title)
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('To-Do', header_text)
 
-assert 'Google' in browser.title
+        # 작업 추가
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(
+            inputbox.get_attribute('placeholder'),
+            '작업 아이템 입력'
+        )
+
+        inputbox.send_keys('공작깃털 사기')
+
+        # 공작 깃털 사기 추가
+        inputbox.send_keys(Keys.ENTER)
+
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_element_by_tag_name('tr')
+        self.assertTrue(
+            any(row.text == '1: 공작깃털 사기' for row in rows),
+        )
+
+
+        self.fail('Finish the test!')
+
+if __name__ == '__main__':
+    unittest.main(warnings='ignore')
--- superlists/lists/tests.py
+++ superlists/lists/tests.py
@@ -1,3 +1,17 @@
+from django.http import HttpRequest, HttpResponse
+from django.urls import resolve
+from lists.views import home_page
 from django.test import TestCase
 
-# Create your tests here.
+class HomePage(TestCase):
+
+    def test_root_url_resolves_to_home_page_view(self):
+        found = resolve('/')
+        self.assertEqual(found.func, home_page)
+
+    def test_home_page_returns_correct_html(self):
+        request = HttpRequest()
+        response = home_page(request)
+        self.assertTrue(response.content.startswith(b'<html>'))
+        self.assertIn(b'<title>To-Do lists</title>', response.content)
+        self.assertTrue(response.content.endswith(b'</html>'))
