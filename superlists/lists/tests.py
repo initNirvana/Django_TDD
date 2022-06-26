13c97a28c0ee1d5ac1fe007e8ffb79aeef465509
Nirvana <kimqqyun@gmail.com>
2022-06-26 18:45:19 +0900

    템블릿, 뷰 리팩터링

--- superlists/lists/tests.py
+++ superlists/lists/tests.py
@@ -1,7 +1,9 @@
+import re
 from django.http import HttpRequest, HttpResponse
 from django.urls import resolve
 from lists.views import home_page
 from django.test import TestCase
+from django.template.loader import render_to_string
 
 class HomePage(TestCase):
 
@@ -12,6 +14,14 @@ class HomePage(TestCase):
     def test_home_page_returns_correct_html(self):
         request = HttpRequest()
         response = home_page(request)
+        expected_html = render_to_string('home.html')
+        self.assertEqual(response.content.decode(), expected_html)
+        
+        
+        """
+        기존 코드
         self.assertTrue(response.content.startswith(b'<html>'))
         self.assertIn(b'<title>To-Do lists</title>', response.content)
-        self.assertTrue(response.content.endswith(b'</html>'))
+        # html 마지막 부분에 공백문제를 위해 strip() 추가
+        self.assertTrue(response.content.strip().endswith(b'</html>'))
+        """
