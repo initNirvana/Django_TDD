499bd0d06b11c74fb65d66bfe42a1fde893735d3
initNirvana <kimqqyun@gmail.com>
2024-01-06 14:06:15 +0900

    목록 출력을 위한 신규 URL, 뷰, 템플릿

--- superlists/functional_tests/tests.py
+++ superlists/functional_tests/tests.py
@@ -5,6 +5,7 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 import unittest
+import time
 
 class NewVisitorTest(LiveServerTestCase):
 
@@ -34,17 +35,71 @@ class NewVisitorTest(LiveServerTestCase):
             '작업 아이템 입력'
         )
 
-        inputbox.send_keys('공작깃털 사기')
+        # 엔터키를 누르면 새로운 URL로 바뀐다. 그리고 작업 목록에 "1: 공작깃털 사기" 아이템이 추가된다.
+        inputbox.send_keys(Keys.ENTER)
+        edith_list_url = self.browser.current_url
+        # assertRegex는 unittest의 헬퍼함수로 정규표현과 문자열이 일치하는지 테스트
+        self.assertRegex(edith_list_url, '/lists/.+') 
 
-        # 공작 깃털 사기 추가
+        time.sleep(2)
+
+        # 엔터키를 치면 페이지가 갱신되고 작업 목록에
+        # "1: 공작깃털 사기" 아이템이 추가된다
         inputbox.send_keys(Keys.ENTER)
 
-        table = self.browser.find_element(By.ID, 'id_list_table')
-        rows = table.find_elements(By.TAG_NAME, 'tr')
-        self.assertTrue(
-            any(row.text == '1: 공작깃털 사기' for row in rows),
-            "신규 작업이 테이블에 표시되지 않는다"
-        )
+        edith_list_url = self.browser.current_url
+        self.assertRegex(edith_list_url,'/lists/.+')
+        self.check_for_row_in_list_table('1: 공작깃털 사기')
 
+        # table = self.browser.find_element(By.ID, 'id_list_table')
+        # rows = table.find_elements(By.TAG_NAME, 'tr')
+        # # self.assertTrue(
+        # #     any(row.text == '1: 공작깃털 사기' for row in rows),
+        # #     "신규 작업이 테이블에 표시되지 않는다 -- 해당 텍스트 :\n%s" % (
+        # #         table.text,
+        # #     )
+        # # )
+        # self.assertIn('1: 공작깃털 사기', [row.text for row in rows])
 
-        self.fail('Finish the test!')
+        # 추가 아이템을 입력할 수 있는 여분의 텍스트 상자가 존재한다.
+        # 다시 "공작깃털을 이용해서 그물 만들기" 라고 입력한다 
+        # (에디스는 매우 체계적인 사람이다)
+        inputbox = self.browser.find_element(By.ID,'id_new_item')
+        inputbox.send_keys('공작깃털을 이용해서 그물 만들기')
+        inputbox.send_keys(Keys.ENTER)
+
+        # 페이지는 다시 갱신되고, 두 개 아이템이 목록에 보인다
+        time.sleep(1)
+        self.check_for_row_in_list_table('1: 공작깃털 사기')
+        self.check_for_row_in_list_table('2: 공작깃털을 이용해서 그물 만들기')
+        
+        # 새로운 사용자인 프란시스가 사이트에 접속한다.
+        ## 새로운 브라우저 세션을 이용해서 에디스의 정보가
+        ## 쿠키를 통해 유입되는 것을 방지한다.
+        self.browser.quit()
+        self.browser = webdriver.Firefox()
+
+        # 프란시스가 홈페이지에 접속한다
+        # 에디스의 리스트는 보이지 않는다
+        self.browser.get(self.live_server_url)
+        page_text = self.browser.find_element(By.TAG_NAME,'body').text
+        self.assertNotIn('공작깃털 사기',page_text)
+        self.assertNotIn('그물 만들기',page_text)
+
+        # 프란시스가 새로운 작업 아이템을 입력하기 시작한다
+        # 그는 에디스보다 재미가 없다
+        inputbox = self.browser.find_element(By.ID,'id_new_item')
+        inputbox.send_keys('우유 사기')
+        inputbox.send_keys(Keys.ENTER)
+
+        # 프란시스가 전용 URL을 취득한다.
+        francis_list_url = self.browser.current_url
+        self.assertRegex(francis_list_url,'/lists/.+')
+        self.assertNotEqual(francis_list_url,edith_list_url)
+
+        # 에디스가 입력한 흔적이 없다는 것을 다시 확인한다.
+        page_text = self.browser.find_element(By.TAG_NAME,'body').text
+        self.assertNotIn('공작깃털 사기',page_text)
+        self.assertIn('우유 사기',page_text)
+
+        # 둘 다 만족하고 잠자리에 든다.
--- superlists/lists/tests.py
+++ superlists/lists/tests.py
@@ -8,6 +8,17 @@ from lists.models import Item
 
 class HomePage(TestCase):
 
+    @staticmethod
+    def remove_csrf(html_code):
+        csrf_regex = r'<input[^>]+csrfmiddlewaretoken[^>]+>'
+        return re.sub(csrf_regex, '', html_code)
+
+    def assertEqualExceptCSRF(self, html_code1, html_code2):
+        return self.assertEqual(
+            self.remove_csrf(html_code1),
+            self.remove_csrf(html_code2)
+        )
+    
     def test_root_url_resolves_to_home_page_view(self):
         found = resolve('/')
         self.assertEqual(found.func, home_page)
@@ -15,8 +26,13 @@ class HomePage(TestCase):
     def test_home_page_returns_correct_html(self):
         request = HttpRequest()
         response = home_page(request)
-        expected_html = render_to_string('home.html')
-        self.assertEqual(response.content.decode(), expected_html)  # type: ignore
+
+        self.assertEqualExceptCSRF(
+            render_to_string('home.html', request=request),
+            response.content.decode()
+        )
+        #expected_html = render_to_string('home.html')
+        #self.assertEqual(response.content.decode(), expected_html)  # type: ignore
 
     def test_home_page_can_save_a_POST_request(self):
         request = HttpRequest()
@@ -37,7 +53,7 @@ class HomePage(TestCase):
         response = home_page(request)
 
         self.assertEqual(response.status_code, 302)
-        self.assertEqual(response['location'], '/')
+        self.assertEqual(response['location'], '/lists/the-only-list-in-the-world/')
 
         """
         self.assertIn('신규 작업 아이템', response.content.decode())  # type: ignore
@@ -63,4 +79,17 @@ class HomePage(TestCase):
         response = home_page(request)
         self.assertEqual(Item.objects.count(), 1)
         new_item = Item.objects.first()
-        self.assertEqual(response['location'], '/')
+        self.assertEqual(response['location'], '/lists/the-only-list-in-the-world/')
+
+    def test_display_all_items(self):
+        Item.objects.create(text='Itemey 1')
+        Item.objects.create(text='Itemey 2')
+
+        response = self.client.get('/lists/the-only-list-in-the-world/')
+
+        self.assertContains(response, 'Itemey 1')
+        self.assertContains(response, 'Itemey 2')
+
+    def test_uses_list_template(self):
+        response = self.client.get('/lists/the-only-list-in-the-world/')
+        self.assertTemplateUsed(response, 'list.html')
